5c3fb71b2f978ad1016d33b1b7844472b55afd4e
bao zhuhan <baozhuhan2004@163.com>
2024-05-25 09:24:29 +0800

    教案和回答潘志文问题的一些实验

---
+++ Class2/认识变量.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	int a = 1; //取值范围大概是正负1e9的数量级 100000000; 
+	int b; 
+	scanf("%d",&b);
+	printf("%d",b);
+	
+	return 0;
+} 
---
+++ Experiment/C语言中的除法.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    int a = 10;
+    int b = 3;
+    printf("%d", 12 + 34);
+    return 0;
+}
